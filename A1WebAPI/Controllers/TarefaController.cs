0361af5bbc1f87b708d7516af583aef54840c7b2
mauriciomarcos.consultoria@gmail.com <mauriciomarcos.consultoria@gmail.com>
2020-02-02 19:07:51 -0300

    Criação da API de tarefas

---
+++ A1WebAPI/Controllers/TarefaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using A1WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A1WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Tarefa>> Get()
+        {
+            var rdn = new Random();
+            var max = rdn.Next(100);
+
+            var listaTarefa = new List<Tarefa>();
+
+            for (int i = 1; i <= max; i++)
+            {
+                var tarefa = new Tarefa()
+                {
+                    Id = i,
+                    Nome = $"Tarefa com a descrião: {i}",
+                    isFinalizado = (i % 2 == 0) ? true : false
+                };
+
+                listaTarefa.Add(tarefa);
+            }
+
+            if (listaTarefa == null)
+            {
+                return NoContent();
+            }
+            if (listaTarefa.Count > 50)
+            {
+                return BadRequest($"Quantidade de tarefas excedo o valor máximo: {listaTarefa.Count}");
+            }
+
+            return Ok(listaTarefa);
+        }
+    }
+}
---
+++ A1WebAPI/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace A1WebAPI.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool isFinalizado { get; set; }
+    }
+}
